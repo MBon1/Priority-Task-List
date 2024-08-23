e04aecaedced641713c12a7e66e7ca49bbe0aeab
Matthew Bonnecaze <bonnecaze.matthew@gmail.com>
2024-08-23 03:29:18 -0400

    Set up Task Model
    Create the Task Model to be used in the backend

--- priority-task-tracker/backend/taskTracker/models.py
+++ priority-task-tracker/backend/taskTracker/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+
+class Task(models.Model):
+    priority = models.IntegerField(
+        default=3,
+        validators=[
+            MaxValueValidator(3),
+            MinValueValidator(1)
+        ]
+    )
+    title = models.CharField(max_length=60)
+    dueDate = models.DateTimeField()
+    description = models.TextField()
+    active = models.BooleanField(default=True)
+
+    def _str_(self):
+        return self.title
